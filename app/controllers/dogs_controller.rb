8ff8d7eeeb640c1aed9353d7e78eacceb8b7c8aa
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-06-13 17:44:47 -0500

    working program. Requires create functionality

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,49 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all
+    end
+
+    def new
+
+    end
+
+    def update
+        # byebug
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params[:dog])
+        redirect_to dogs_path
+    end
+
+    def create
+
+    end
+
+    def show 
+        @dog = Dog.find(params[:id])
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to dogs_path
+    end
+
+    def dog_params
+        params.permit(
+            
+            dog: [
+                :name,
+                :breed,
+                :age,
+                employee_ids: []
+            ]
+            
+        )
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,46 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def new
+
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to employees_path
+    end
+
+    def create
+
+    end
+
+    def show 
+        @employee = Employee.find(params[:id])
+    end
+
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    def employee_params
+        params.permit(
+            :first_name,
+            :last_name,
+            :alias,
+            :title,
+            :office,
+            :dog_id,
+            :img_url
+        )
+    end
 end
